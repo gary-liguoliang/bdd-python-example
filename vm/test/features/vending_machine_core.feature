fd8f20c0910b52bcd0d182ca335e7033192c3542
Guoliang Li <guoliang.me@gmail.com>
2016-06-05 15:27:00 +0800

    committed unittest.

--- vm/test/features/vending_machine_core.feature
+++ vm/test/features/vending_machine_core.feature
@@ -1,7 +1,7 @@
 Feature: buy item
   as a customer, I want to get the item after I pay enough money.
 
-  Scenario: buy an item with exact amount
+  Scenario: customer buys an item with exact amount
     Given a working vending machine
       and "Coca-Cola" is available with price 1.20
     When I selected "Coca-Cola"
--- vm/test/test_vendingMachine.py
+++ vm/test/test_vendingMachine.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
 
+from vm.vending_machine import VendingMachine
+
 
 class TestVendingMachine(TestCase):
-    def test_add_to_inventory(self):
-        self.fail()
+
+    def setUp(self):
+        super(TestVendingMachine, self).setUp()
+        self.vending_machine = VendingMachine()
 
     def test_sell_item(self):
-        self.fail()
+        item_name = "Coca"
+        self.vending_machine.add_to_inventory(item_name, 1.0)
+        item_received = self.vending_machine.sell_item("%s" % item_name, 1.0)
+        self.assertIsNotNone(item_received)
