869c13215b0c75053250ae8bb7b00f2066f40b1b
Guoliang Li <guoliang.me@gmail.com>
2016-06-05 20:05:26 +0800

    added build scripts.

---
+++ bin/install_and_run_at.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+
+virtualenv venv
+. venv/bin/activate
+
+pip install -r requirements.txt
+pip install -e .
+
+lettuce vm/test/ --with-xunit
---
+++ bin/install_and_run_unittest.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+
+virtualenv venv
+. venv/bin/activate
+
+pip install -r requirements.txt
+pip install -e .
+
+nosetests --with-xunit --with-coverage --cover-xml
